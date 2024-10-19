2271ee500e0f273e445415e7f1667084b3aa95d3
nskowron <nel.skowronek@gmail.com>
2024-10-19 14:00:17 +0000

    Initial cmake setup

---
+++ cpp/include/functions.hpp
@@ -0,0 +1,8 @@
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
+
+double f(double x);
+double g(double x);
+double h(double x);
+
+#endif
---
+++ cpp/main/main.cpp
@@ -0,0 +1,8 @@
+#include <functions.hpp>
+
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Hello World!";
+}
