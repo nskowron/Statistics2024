334ec4873017676386bdb29d5ddb1072f3ca0f00
nskowron <nel.skowronek@gmail.com>
2024-11-27 17:06:21 +0000

    Changed y-axis limit for plot

--- Exc2/cpp/src/plot.cpp
+++ Exc2/cpp/src/plot.cpp
@@ -9,6 +9,10 @@
 
 void plot(axis<double> result, axis<double> avg_result, std::string file)
 {
+    auto y_lim = matplot::gca()->y2lim();
+    y_lim.at(0) = 0;
+    matplot::ylim(y_lim);
+
     matplot::scatter(result.x, result.y)->marker(".").marker_size(0.5).color("blue");
     matplot::hold(matplot::on);
     matplot::plot(avg_result.x, avg_result.y)->line_width(2).color("red");
@@ -19,6 +23,10 @@ void plot(axis<double> result, axis<double> avg_result, std::string file)
 
 void plot_line(axis<double> result, std::string file)
 {
+    auto y_lim = matplot::gca()->y2lim();
+    y_lim.at(0) = 0;
+    matplot::ylim(y_lim);
+
     matplot::plot(result.x, result.y)->line_width(2).color("red");
 
     matplot::save(file);
