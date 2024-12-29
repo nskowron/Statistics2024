a56f9b9f1a2ae9420c056468032e462c7e6f8331
nskowron <nel.skowronek@gmail.com>
2024-12-29 19:38:45 +0000

    Asymptotic Tn

--- Exc3/Exc3/cpp/main/main.cpp
+++ Exc3/Exc3/cpp/main/main.cpp
@@ -6,40 +6,41 @@
 #include <algorithm>
 #include <cmath>
 
-void process_results(full_simulation_results& results, const std::string& file_base);
+void process_results(full_simulation_results& results, const std::string& file_base, double p);
 
 int main()
 {
     {
+        double p = 0.5;
         std::string file_base = std::string(OUTPUT_DIRECTORY) + "Plot_5_";
-        full_simulation_results results = full_simulate_broadcast(50, 0.5);
-        process_results(results, file_base);
+        full_simulation_results results = full_simulate_broadcast(50, p);
+        process_results(results, file_base, p);
     }
 
     {
+        double p = 0.1;
         std::string file_base = std::string(OUTPUT_DIRECTORY) + "Plot_1_";
-        full_simulation_results results = full_simulate_broadcast(50, 0.1);
-        process_results(results, file_base);
+        full_simulation_results results = full_simulate_broadcast(50, p);
+        process_results(results, file_base, p);
     }
 }
 
-void process_results(full_simulation_results& results, const std::string& file_base)
+void process_results(full_simulation_results& results, const std::string& file_base, double p)
 {
     plot(results.T, results.avg_T, file_base + "T.pdf");
     plot(results.Tn, results.avg_Tn, file_base + "Tn.pdf");
 
-    //plot(results.Tn.at(0), results.avg_Tn.at(0), file_base + "")
-    // { // c) cmp(n)/n
-    //     axis<double> result;
-    //     result.x = results.avg_cmp.x;
-    //     std::transform(
-    //         results.avg_cmp.y.begin(),
-    //         results.avg_cmp.y.end(),
-    //         results.avg_cmp.x.begin(),
-    //         std::back_inserter(result.y),
-    //         [](auto& c, auto& n){return c / n;}
-    //     );
+    { // test) T(n)/(ln n / -ln(1-p))
+        axis<double> result;
+        result.x = results.avg_T.x;
+        std::transform(
+            results.avg_T.y.begin(),
+            results.avg_T.y.end(),
+            results.avg_T.x.begin(),
+            std::back_inserter(result.y),
+            [p](auto& t, auto& n){return t / (log(n) / -log(1-p));}
+        );
 
-    //     plot_line(result, file_base + "c)-1.pdf");
-    // }
+        plot_line(result, file_base + "test)-1.pdf");
+    }
 }
