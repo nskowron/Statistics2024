886868c5221b5aeac7113e1df456cbc754fb4852
nskowron <nel.skowronek@gmail.com>
2024-12-30 19:12:35 +0000

    Added Tni PMF

--- Exc3/Exc3/cpp/main/main.cpp
+++ Exc3/Exc3/cpp/main/main.cpp
@@ -30,7 +30,7 @@ void process_results(full_simulation_results& results, const std::string& file_b
     plot(results.Tn, results.avg_Tn, file_base + "Tn.pdf");
     plot(results.Tn1, results.avg_Tn1, file_base + "Tn1.pdf");
 
-    { // test) T(n)/(ln n / -ln(1-p))
+    { // Tn asym) T(n)/(ln n / -ln(1-p))
         axis<double> result;
         result.x = results.avg_Tn.x;
         std::transform(
@@ -41,6 +41,27 @@ void process_results(full_simulation_results& results, const std::string& file_b
             [p](auto& t, auto& n){return t / (log(n) / -log(1-p));}
         );
 
-        plot_line(result, file_base + "test)-1.pdf");
+        plot_line(result, file_base + "Tn_asym)-1.pdf");
+    }
+
+    { // PMF of Tni
+        axis<int> val_count;
+        
+        val_count.y.resize(3 * (1/p), 0); //3 times expected value should be enough
+        for(auto val : results.Tn1.y)
+        {
+            if(val <= val_count.y.size())
+            {
+                ++val_count.y.at(val - 1);
+            }
+        }
+
+        val_count.x.resize(3 * (1/p), 0);
+        for(int i = 0; i < val_count.x.size(); ++i)
+        {
+            val_count.x.at(i) = i + 1;
+        }
+
+        plot_line(val_count, file_base + "Tni_PMF.pdf");
     }
 }
