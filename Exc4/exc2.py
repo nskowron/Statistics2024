faeb8513828ff14febc82e6e90e152616cd457e5
nskowron <nel.skowronek@gmail.com>
2025-01-30 21:44:53 +0000

    changed plots hierarchy

--- Exc4/exc2.py
+++ Exc4/exc2.py
@@ -5,23 +5,23 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
 
-with PdfPages("plots/exc2.pdf") as pdf:
-    for n in [10, 15, 20, 25, 30, 100]:
-        x = np.arange(0, n+1)
-        cdf = stats.binom.cdf(x, n, .5)
-        E = stats.binom.mean(n, .5)
-        var = stats.binom.var(n, .5)
+for n in [10, 15, 20, 25, 30, 100]:
+    x = np.arange(0, n+1)
+    cdf = stats.binom.cdf(x, n, .5)
+    E = stats.binom.mean(n, .5)
+    var = stats.binom.var(n, .5)
 
-        # Sn is a linear transformation of Bin(n, .5)
-        tr_x = 2 * x - n
-        tr_cdf = cdf # cdf y stays the same
-        tr_E = 2 * E - n
-        tr_var = 4 * var
+    # Sn is a linear transformation of Bin(n, .5)
+    tr_x = 2 * x - n
+    tr_cdf = cdf # cdf y stays the same
+    tr_E = 2 * E - n
+    tr_var = 4 * var
 
-        # Approximating with normal distribution
-        norm_x = np.linspace(-n, n, 1000)
-        norm_cdf = stats.norm.cdf(norm_x, tr_E, math.sqrt(tr_var))
+    # Approximating with normal distribution
+    norm_x = np.linspace(-n, n, 1000)
+    norm_cdf = stats.norm.cdf(norm_x, tr_E, math.sqrt(tr_var))
 
+    with PdfPages(f"plots/exc2/n{n}.pdf") as pdf:
         plt.figure()
         plt.title('N = ' + str(n))
         plt.bar(tr_x[:-1], tr_cdf[:-1], width=np.diff(tr_x), align='edge', color='royalblue', label='Sn CDF')
--- Exc4/exc3.py
+++ Exc4/exc3.py
@@ -3,18 +3,18 @@ import scipy.stats as stats
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
-with PdfPages("plots/exc3.pdf") as pdf:
-    for n in [100, 1000, 10000]:
-        P = []
-        for k in range(1,5001):
-            L = 0 # time spent above 0
-            S = 0 # initial position
-            for X in stats.bernoulli.rvs(.5, size=n) * 2 - 1: # tossing coin n times and linear transformation to get -1, 1
-                if S > 0 or S + X > 0:
-                    L += 1
-                S += X
-            P.append(L / n)
+for n in [100, 1000, 10000]:
+    P = []
+    for k in range(1,5001):
+        L = 0 # time spent above 0
+        S = 0 # initial position
+        for X in stats.bernoulli.rvs(.5, size=n) * 2 - 1: # tossing coin n times and linear transformation to get -1, 1
+            if S > 0 or S + X > 0:
+                L += 1
+            S += X
+        P.append(L / n)
         
+    with PdfPages(f"plots/exc3/n{n}.pdf") as pdf:
         hist, edges = np.histogram(P, bins=20, density=True)
         plt.figure()
         plt.title(f'N = {n}')
@@ -26,7 +26,7 @@ with PdfPages("plots/exc3.pdf") as pdf:
         pdf.savefig()
         plt.close()
 
-with PdfPages("plots/exc3-arcsin.pdf") as pdf:
+with PdfPages("plots/exc3/arcsin.pdf") as pdf:
     def arcsin_pdf(x):
         return 1 / (np.pi * np.sqrt(x * (1 - x)))
     
